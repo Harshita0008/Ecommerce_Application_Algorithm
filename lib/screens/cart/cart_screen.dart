8d814aefcce68ffbd53b51a5e433c1f363bfe917
Harshita <harshitathakur0008@gmail.com>
2022-05-29 20:10:28 +0530

    my project

---
+++ lib/screens/cart/cart_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/models/Cart.dart';
+
+import 'components/body.dart';
+import 'components/check_out_card.dart';
+
+class CartScreen extends StatelessWidget {
+  static String routeName = "/cart";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(context),
+      body: Body(),
+      bottomNavigationBar: CheckoutCard(),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      title: Column(
+        children: [
+          Text(
+            //all selected items will fall in cart category
+            "Cart",
+            style: TextStyle(color: Colors.black),
+          ),
+          Text(
+            "${demoCarts.length} items",
+            style: Theme.of(context).textTheme.caption,
+          ),
+        ],
+      ),
+    );
+  }
+}
